4be3c38eec6a6138957f2a4d3befc56fd706e9d2
Shreyas Mididoddi <me@shreyascodes.tech>
2024-11-21 02:12:04 +0530

    feat: created a basic go fiber app

--- main.go
+++ main.go
@@ -2,8 +2,32 @@ package main
 
 import (
 	"fmt"
+	"log"
+
+	goenv "github.com/Netflix/go-env"
+
+	"github.com/gofiber/fiber/v3"
+	"github.com/gofiber/fiber/v3/middleware/logger"
 )
 
+type Env struct {
+	Port string `env:"PORT,required,default=3000"`
+}
+
 func main() {
-	fmt.Println("Hello World")
+	app := fiber.New()
+	app.Use(logger.New())
+
+	var env Env
+	_, err := goenv.UnmarshalFromEnviron(&env)
+	if err != nil {
+		log.Fatalf("Could not load environment variables: %s", err)
+	}
+
+	app.Get("/", func(c fiber.Ctx) error {
+		fmt.Println(c.Host())
+		return c.SendString("Hello, World!")
+	})
+
+	app.Listen(":" + env.Port)
 }
